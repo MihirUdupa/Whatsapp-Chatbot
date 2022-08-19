ea7852812ae397332c30e17acec3ad6dba02abcf
Mihir <mihir@iotracx.com>
2022-08-19 17:59:30 +0530

    added some db functionality to bot server

--- bot-backend/bot.js
+++ bot-backend/bot.js
@@ -2,24 +2,40 @@ const express = require('express');
 const axios = require('axios')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config()
-const app = express()
+require('dotenv').config();
+const app = express();
+const mongoClient = require('mongodb').MongoClient;
 
 const waToken = process.env.WATOKEN;
 const verify_token = process.env.VERIFY_TOKEN;
+const mongo_Uri = process.env.MONGO_URI;
+const client = new mongoClient(mongo_Uri, {useNewUrlParser: true, useUnifiedTopology: true})
+let collection
 
 function initialize() {
     app.use(express.static('pages'));
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(bodyParser.json());
     app.use(cors());
+    client.connect(err => {
+        if(err) {
+            console.log(err);
+            client.close();
+        } else {
+            collection = client.db("Wa_Test_Messages").collection("Whatsapp");
 
-    app.get('/chatbot/validation',function(req,res) {
-        handelValidation(req, res);
-    });
-
-    app.post('/chatbot/validation', function(req, res) {
-        handelGetUserInput(req, res);
+            app.get('/chatbot/validation',function(req,res) {
+                handelValidation(req, res);
+            });
+        
+            app.post('/chatbot/validation', function(req, res) {
+                handelGetUserInput(req, res);
+            })
+        
+            app.post('/chatbot/getMessage', function(req, res) {
+                handelGetAllMessages(req.body, res);
+            })
+        }
     })
 }
 
@@ -76,13 +92,18 @@ async function handelGetUserInput(req, res) {
         let data = {}
         if(body.entry && body.entry[0].changes && body.entry[0].changes[0] && body.entry[0].changes[0].value.messages && body.entry[0].changes[0].value.messages[0]) {
             phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
-            from = req.body.entry[0].changes[0].value.messages[0].from;
             type = req.body.entry[0].changes[0].value.messages[0].type;
             if(type === "text") {
+                let Usermessage = req.body.entry[0].changes[0].value.messages[0].text.body;
+                let Botmessage = "";
                 msg_body_switcher = req.body.entry[0].changes[0].value.messages[0].text.body;
+                let time = getCurrentTimestamp();
+                from = req.body.entry[0].changes[0].value.messages[0].from;
+                sendToDB(from, Usermessage, Botmessage,time);
                 msg_body = switcher(msg_body_switcher);
             } else if (type === "button") {
                 msg_body = req.body.entry[0].changes[0].value.messages[0].button.text;
+                from = req.body.entry[0].changes[0].value.messages[0].from;
             }
             data.message = msg_body
 
@@ -95,6 +116,10 @@ async function handelGetUserInput(req, res) {
                 data : data.message
             }).then(async (result) => {
                 if(result.data.result) {
+                    let Botmessage = result.data.result;
+                    let Usermessage = "";
+                    let time = getCurrentTimestamp();
+                    sendToDB(from, Usermessage, Botmessage, time);
                     await axios({
                         method: 'post',
                         url : process.env.USER_URL + phone_number_id + "/messages?access_token=" + waToken,
@@ -169,4 +194,37 @@ function switcher(messages) {
             return messages;
             break;
     }
+}
+
+function sendToDB (from, Usermessage, Botmessage, time) {
+    let inputData = {
+        "Phone_Number" : from,
+        "User_Messages": Usermessage,
+        "Bot_Messages": Botmessage,
+        "DateTime": time
+    }
+    collection.insertOne(inputData).then((result) => {
+        console.log(result);
+    }).catch((err) => {
+        console.log(err);
+    })
+}
+
+function getCurrentTimestamp() {
+    return new Date().getTime();
+}
+
+function handelGetAllMessages(number, res) {
+    let query = {
+        "Phone_Number" : number.phone,
+    }
+    collection.find(query, {projection:{_id:0}}).sort({_id:-1}).toArray().then((result) => {
+        if(result.length > 0) {
+            res.json({"response_desc":"success","response_data":result,"response_code":"0"})
+        } else {
+            res.json({"response_desc":"success","response_data":{},"response_code":"1"})
+        }
+    }).catch((err) => {
+        res.json({"response_desc":"Internal Server Error","response_data":err,"response_code":"500"})
+    })
 }
